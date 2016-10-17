b67b0e8035c133e923073fbd508764d8e2fa63b6
John <johngodsey@gmail.com>
2016-10-16 20:18:48 -0400

    Added servo constants from my old pololu maestro settings

--- esra.ino
+++ esra.ino
@@ -1,3 +1,33 @@
+const int mouthMax = 2464;
+const int mouthMin = 992;
+
+const int lipsMax = 2464;
+const int lipsMin = 992;
+
+const int eyesYMin = 1232;
+const int eyesYMax = 2528;
+
+const int eyesXMin = 512;
+const int eyesXMax = 2000;
+
+const int eyebrowsMin = 496;
+const int eyebrowsMax = 1504;
+
+const int neckMin = 800;
+const int neckMax = 2192;
+
+const int shoulderRightMin = 992;
+const int shoulderRightMax = 2368;
+
+const int elbowRightMin = 992;
+const int elbowRightMax = 2208;
+
+const int shoulderLeftMin = 672;
+const int shoulderLeftMax = 2000;
+
+const int elbowLeftMin = 992;
+const int elbowLeftMax = 2000;
+
 void setup() {
   // put your setup code here, to run once:
 
